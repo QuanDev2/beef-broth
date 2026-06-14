58dd3ae01308feeaf35a15091b7f6b9e93f4fe2b
Quan <qnguyen.dev2@gmail.com>
2026-06-14 17:59:27 -0400

    optional system

--- sandbox/llm.py
+++ sandbox/llm.py
@@ -1,18 +1,18 @@
 import anthropic
 from dotenv import load_dotenv
+from typing import Optional
 
 load_dotenv()
 
 client = anthropic.Anthropic()
 
 
-def call_llm(messages: list[dict], temperature: float = 0, max_tokens: int = 256) -> str:
-    response = client.messages.create(
-        model="claude-haiku-4-5-20251001",
-        max_tokens=max_tokens,
-        messages=messages,
-        temperature=temperature
-    )
+def call_llm(messages: list[dict], temperature: float = 0, max_tokens: int = 256, system: Optional[str] = None) -> str:
+    kwargs = dict(model="claude-haiku-4-5-20251001", max_tokens=max_tokens, messages=messages, temperature=temperature)
+    if system:
+        kwargs["system"] = system
+    response = client.messages.create(**kwargs)
+
     print("input tokens", response.usage.input_tokens)
     print("output tokens", response.usage.output_tokens)
     input_cost  = response.usage.input_tokens  * (0.80 / 1_000_000)
@@ -28,18 +28,13 @@ if __name__ == "__main__":
         "role": "user",
         "content": "Describe Saigon in one sentence."
     }]
-    print("\n--- Temp 0 ---")
-    for _ in range(3):
-        rep1 = call_llm(prompt)
-        print(rep1) 
+    print("\n--- No system ---")
+    rep1 = call_llm(prompt)
+    print(rep1) 
 
-    print("\n--- Temp 1 ---")
-    for _ in range(3):
-        rep1 = call_llm(prompt, temperature=1)
-        print(rep1) 
-
-    
-
+    print("\n--- With system ---")
+    rep1 = call_llm(prompt, system="You're a travel ghostwriter. Use only facts. Do not invent anything")
+    print(rep1) 
 
 
 
